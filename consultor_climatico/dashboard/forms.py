f91a9eb87aabc6157becff96988fb1fda71f454a
Yuri Dias <yuridias2200@gmail.com>
2025-02-20 17:37:56 -0300

    Add functionality to add cities to user profile from dashboard

--- consultor_climatico/dashboard/forms.py
+++ consultor_climatico/dashboard/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Cidade
+
 
 class RegistroForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +10,10 @@ class RegistroForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class AdicionarCidadeForm(forms.Form):
+    cidade = forms.ModelChoiceField(
+        queryset=Cidade.objects.all(),
+        label="Escolha uma cidade",
+        required=True
+    )
--- consultor_climatico/dashboard/urls.py
+++ consultor_climatico/dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import dashboard, registrar, logar
+from .views import dashboard, registrar, logar, adicionar_cidade, remover_cidade
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path("adicionar_cidade/", adicionar_cidade, name="adicionar_cidade"),
+    path('remover_cidade/<int:cidade_id>/', remover_cidade, name='remover_cidade'),
     path('registrar/', registrar, name='registrar'),
     path('login/', logar, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
